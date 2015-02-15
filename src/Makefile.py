28ebcf143f0f98b7ac635dfb6b9027ec00b4bd79
Dmitry Sovetov <dmsovetov@gmail.com>
2015-02-15 17:03:37 +0200

    Fixed makefile

--- src/Makefile.py
+++ src/Makefile.py
@@ -1,4 +1,4 @@
-core = StaticLibrary( 'Core', include = ['../dependency', '.'], sources = [ '.', 'utils/*' ], defines = [ 'DC_BUILD_LIBRARY' ] )
+core = StaticLibrary( 'Core', paths = ['../dependency', '.'], sources = [ '.', 'utils/*' ], defines = [ 'DC_BUILD_LIBRARY' ] )
 if Has( 'threads' ):  core.dirs( 'threads', 'threads/Task', 'threads/$(THREADS)' )
 if Has( 'sound' ):    core.dirs( 'sound', 'sound/Drivers', 'sound/Decoders', 'sound/Drivers/$(SOUND)' )
 if Has( 'renderer' ): core.dirs( 'renderer', 'renderer/$(RENDERER)', 'renderer/$(RENDERER)/$(PLATFORM)' )
