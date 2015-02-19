dde65c7b75ecf09f4ce87e73853c53c52eb5248c
Dmitry Sovetov <dmsovetov@gmail.com>
2015-02-19 09:07:55 +0200

    Modified platform key struct

--- src/dreemchest/platform/Platform.h
+++ src/dreemchest/platform/Platform.h
@@ -39,20 +39,24 @@ namespace platform {
 
     // ** enum Key
     //! Available key mappings.
-    enum Key {
-        _0, _1, _2, _3, _4, _5, _6, _7, _8, _9,
-        Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
-        A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
-        F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
-        Escape, Tab, Capital, Shift, Control, Win, Alt, Space,
-        Return, Back,
-        Pause, Snapshot, Scroll,
-        PgDown, PgUp, Home, End, Insert, Delete,
-        Up, Down, Left, Right,
-        Numlock, Divide, Multiply, Subtract, Add, Decimal,
-        RButton, LButton, MButton,
+    struct Key {
+		enum Mapping {
+			_0, _1, _2, _3, _4, _5, _6, _7, _8, _9,
+			Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
+			A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+			F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+			Escape, Tab, Capital, Shift, Control, Win, Alt, Space,
+			Return, Back,
+			Pause, Snapshot, Scroll,
+			PgDown, PgUp, Home, End, Insert, Delete,
+			Up, Down, Left, Right,
+			Numlock, Divide, Multiply, Subtract, Add, Decimal,
+			RButton, LButton, MButton,
 
-        Total
+			Total
+		} m_mapping;
+
+		Key( Mapping mapping ) : m_mapping( mapping ) {}
     };
 
     class Application;
