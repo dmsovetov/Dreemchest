60936affd45377ffe4bb543686dc439ba9ed1a10
Dmitry Sovetov <DSovetov@dm.local>
2015-02-23 17:16:59 +0200

    Fixed example makefile

--- src/examples/Makefile.py
+++ src/examples/Makefile.py
@@ -1,4 +1,13 @@
 examples = Executable( 'Examples', paths = [ '../dreemchest' ], link = [ 'Dreemchest' ] )
 
-if Has( 'platform' ): examples.dirs( '01_Platform/*' )
-if Has( 'renderer' ): examples.dirs( '02_Renderer/*' )
+if Has( 'platform' ):
+	examples.dirs( '01_Platform/*' )
+
+	if platform == 'iOS':
+		examples.linkExternal( Library( 'Foundation', True ) )
+		examples.linkExternal( Library( 'UIKit', True ) )
+	elif platform == 'MacOS':
+		examples.linkExternal( Library( 'Cocoa', True ) )
+
+if Has( 'renderer' ):
+	examples.dirs( '02_Renderer/*' )
