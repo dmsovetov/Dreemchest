cdb1dc37051b5691e3cd491bd742a6422cd9b627
sovetov <,jitynevfq51>
2015-03-18 15:35:24 +0200

    Modified configure command

--- tools/configure-win32.bat
+++ tools/configure-win32.bat
@@ -1,2 +1,2 @@
-python -m Pygling configure --output ../projects/ --source ../src --name dreemchest --platform Windows --io=yes --threads=Windows
+python -m Pygling configure --output ../projects/ --source ../src --name dreemchest --platform Windows --io=yes --threads=Windows --renderer=OpenGL
 pause
