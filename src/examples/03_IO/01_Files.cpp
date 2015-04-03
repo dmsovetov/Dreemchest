443810fe75d998ce7647f686d873c105ebd990e6
sovetov <jitynevfq51>
2015-04-03 18:57:27 +0300

    -

--- src/examples/03_IO/01_Files.cpp
+++ src/examples/03_IO/01_Files.cpp
@@ -41,86 +41,6 @@ using namespace platform;
 // Open a io namespace.
 using namespace io;
 
-class Ar : public RefCounted {
-public:
-
-	virtual			~Ar( void ) {}
-};
-
-class BinaryArchive : public Ar {
-public:
-
-	template<typename T>
-	void write( const T& value )
-	{
-		write( &value, sizeof( T ) );
-	}
-
-	template<typename T>
-	void read( T& value ) const
-	{
-		read( &value, sizeof( T ) );
-	}
-
-	virtual void write( const void* ptr, u32 size )
-	{
-		printf( "Writing %d bytes\n", size );
-	}
-
-	virtual void read( void* ptr, u32 size ) const
-	{
-		printf( "Reading %d bytes\n", size );
-	}
-};
-
-typedef StrongPtr<KeyValueArchive> KeyValueArchivePtr;
-
-class KeyValueArchive : public Ar {
-public:
-
-	virtual void write( CString key, const Variant& value )
-	{
-		printf( "Writing %s as %s\n", key, value.typeName() );
-	}
-
-	virtual Variant read( CString key ) const
-	{
-		printf( "Reading %s\n", key );
-		return Variant();
-	}
-
-	virtual KeyValueArchivePtr object( CString key )
-	{
-		return KeyValueArchivePtr( new KeyValueArchive );
-	}
-
-	virtual KeyValueArchivePtr object( CString key ) const
-	{
-		return KeyValueArchivePtr( new KeyValueArchive );
-	}
-
-	virtual KeyValueArchivePtr array( CString key )
-	{
-		return KeyValueArchivePtr( new KeyValueArchive );
-	}
-
-	virtual KeyValueArchivePtr array( CString key ) const
-	{
-		return KeyValueArchivePtr( new KeyValueArchive );
-	}
-};
-
-#define _IoBeginSerializer								\
-	static SerializerList serializers( Type* value ) {	\
-		SerializerList result;
-
-#define _IoField( name )	\
-	result.push_back( Serializer::create( #name, value->name ) );
-
-#define _IoEndSerializer	\
-		return result;		\
-	}
-
 struct A {
 	float b;
 };
@@ -129,30 +49,10 @@ struct Nested : public SerializableType<Nested> {
 	String			m_a;
 	int				m_b;
 
-	_IoBeginSerializer
-		_IoField( m_a )
-		_IoField( m_b )
-	_IoEndSerializer
-
-	template<typename T>
-	void write( T* ar )
-	{
-		SerializerList fields = serializers( this );
-
-		for( SerializerList::iterator i = fields.begin(), end = fields.end(); i != end; ++i ) {
-			i->get()->write( ar );
-		}
-	}
-
-	template<typename T>
-	void read( const T* ar )
-	{
-		SerializerList fields = serializers( this );
-
-		for( SerializerList::iterator i = fields.begin(), end = fields.end(); i != end; ++i ) {
-			i->get()->read( ar );
-		}
-	}
+	IoBeginSerializer
+		IoField( m_a )
+		IoField( m_b )
+	IoEndSerializer
 };
 
 struct Data : public SerializableType<Data> {
@@ -164,35 +64,15 @@ struct Data : public SerializableType<Data> {
 	Array<double>	m_doubles;
 	Array<Nested>	m_nestedItems;
 
-	template<typename T>
-	void write( T* ar )
-	{
-		SerializerList fields = serializers( this );
-
-		for( SerializerList::iterator i = fields.begin(), end = fields.end(); i != end; ++i ) {
-			i->get()->write( ar );
-		}
-	}
-
-	template<typename T>
-	void read( const T* ar )
-	{
-		SerializerList fields = serializers( this );
-
-		for( SerializerList::iterator i = fields.begin(), end = fields.end(); i != end; ++i ) {
-			i->get()->read( ar );
-		}
-	}
-
-	_IoBeginSerializer
-		_IoField( m_string )
-		_IoField( m_boolean )
-		_IoField( m_integer )
-		_IoField( m_float )
-		_IoField( m_nested )
-		_IoField( m_doubles )
-		_IoField( m_nestedItems )
-	_IoEndSerializer
+	IoBeginSerializer
+		IoField( m_string )
+		IoField( m_boolean )
+		IoField( m_integer )
+		IoField( m_float )
+		IoField( m_nested )
+		IoField( m_doubles )
+		IoField( m_nestedItems )
+	IoEndSerializer
 };
 
 // Application delegate is used to handle an events raised by application instance.
@@ -204,42 +84,39 @@ class Files : public ApplicationDelegate {
         platform::log::setStandardHandler();
         io::log::setStandardHandler();
 
-		Variant integer( 1 );
-		Variant boolean( true );
-		Variant number( 213.23f );
-		Variant str( "hello" );
+		Data d1, d2, d3;
+		d1.m_boolean = true;
+		d1.m_integer = -23;
+		d1.m_float   = 2.23f;
+		d1.m_string = "str";
+		d1.m_doubles.push_back( 1.0 );
+		d1.m_doubles.push_back( 1.111 );
+		d1.m_nested.m_a = "hello";
+		d1.m_nested.m_b = 23;
+		d1.m_nestedItems.push_back( d1.m_nested );
+		d1.m_nestedItems.push_back( d1.m_nested );
 
-		printf( "%s\n", integer.toCString() );
+		ByteBufferPtr stream = ByteBuffer::create();
+		BinaryStorage binary( stream );
+		JsonStorage   json;
 
-		f64 a = number.as<f64>();
-		String s = str.as<String>();
-		s32 i = str.as<s32>();
+		d1.write( &binary );
+		stream->setPosition( 0 );
+		d2.read( &binary );
 
-		Data d;
-
-		d.m_string = "hello";
-		d.m_nestedItems.push_back( Nested() );
-		d.m_nestedItems.push_back( Nested() );
-		d.m_doubles.push_back( 1.0 );
-		d.m_doubles.push_back( 2323.9 );
-		d.m_doubles.push_back( 23.0321 );
-
-	//	d.write( new BinaryArchive );
-		d.write( new KeyValueArchive );
-
+		d1.write( &json );
+		printf( "%s\n", json.toString().c_str() );
+		d3.read( &json );
 /*
-		Data z;
+		Nested n1, n2;
+		n1.m_a = "wow";
+		n1.m_b = 23;
 
-		foo2( "m_boolean", z.m_boolean );
-		foo2( "m_integer", z.m_integer );
-		foo2( "m_float", z.m_float );
-		foo2( "m_a", z.m_a );
-		foo2( "m_string", z.m_string );
-		foo2( "m_xx", z.m_xx );
-		foo2( "m_nested", z.m_nested );
-		foo2( "m_doubles", z.m_doubles );
-		foo2( "m_nestedItems", z.m_nestedItems );
-*/
+		JsonStorage json;
+		n1.write( &json );
+
+		printf( "%s\n", json.toString().c_str() );
+	*/
     }
 };
 
