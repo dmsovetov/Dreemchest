c68adafb4aa71cecf020d3ca6074649bc28f7284
sovetov <jitynevfq51>
2015-04-29 20:06:30 +0300

    Fixed shader

--- src/dreemchest/scene/Renderer.cpp
+++ src/dreemchest/scene/Renderer.cpp
@@ -63,7 +63,7 @@ Renderer::Renderer( renderer::Hal* hal ) : m_hal( hal )
 			void main()
 			{
                 vec4 light   = texture2D( u_lightmap, v_tex1 );
-				gl_FragColor = texture2D( u_diffuse, v_tex0 ) * (light * 2.0) * u_diffuseColor + vec4( 0.5254902, 0.80392157, 0.91764706 );
+				gl_FragColor = texture2D( u_diffuse, v_tex0 ) * (light * 2.0) * u_diffuseColor + vec4( 0.5254902, 0.80392157, 0.91764706, 1.0 );
 			} ) );
 
 	// ** Create the colored & textured shader
