71f74998a371f5e01a1c3db04de9b6b7c30a5d26
Dmitry Sovetov <dmsovetov@gmail.com>
2015-09-30 18:56:19 +0300

    Added WeakPtr != operator with other WeakPtr instance

--- src/dreemchest/Base/memory/WeakPtr.h
+++ src/dreemchest/Base/memory/WeakPtr.h
@@ -73,6 +73,7 @@ DC_BEGIN_DREEMCHEST
         bool                operator == ( T *pointer ) const;
 		bool				operator == ( const WeakPtr<T>& other ) const;
         bool                operator != ( T *pointer ) const;
+		bool				operator != ( const WeakPtr<T>& other ) const;
     };
 
     // ** WeakPtr::WeakPtr
@@ -153,6 +154,13 @@ DC_BEGIN_DREEMCHEST
     bool WeakPtr<T>::operator != ( T *pointer ) const {
         manageProxy();
         return ( m_ptr != pointer );
+    }
+
+    // ** WeakPtr::operator !=
+    template<typename T>
+    bool WeakPtr<T>::operator != ( const WeakPtr<T>& other ) const {
+        manageProxy();
+        return ( m_ptr != other.get() );
     }
 
     // ** WeakPtr::setPointer
