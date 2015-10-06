a0ada8e119d30d4b1458f2990f9915377338d714
Dmitry Sovetov <dmsovetov@gmail.com>
2015-10-06 16:39:45 +0300

    Removed old makefiles

--- src/dreemchest/renderer/Makefile.py
+++
@@ -1,6 +0,0 @@
-dRenderer = StaticLibrary( 'dRenderer', sources = [ '.', '$(RENDERER)', '$(RENDERER)/$(PLATFORM)' ], defines = [ 'DC_BUILD_LIBRARY' ] )
-
-Library( Get( 'renderer' ), True )
-
-if iOS:
-	dRenderer.linkExternal( Library( 'QuartzCore', True ) )
