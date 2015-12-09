bfe2e512471711864dcf33952094dc01e3dc8502
Dmitry Sovetov <dmsovetov@gmail.com>
2015-12-09 11:54:04 +0200

    Improved Qt configuration options

--- tools/env.bat
+++ tools/env.bat
@@ -2,5 +2,8 @@
 
 set PROJECTS=..\Projects
 set QT4=D:\SDK\Qt\4.8.6
-set QT5=
+set QT5=D:\SDK\Qt\5.5
+set CMAKEHOME=C:\Program Files (x86)\CMake\bin
+
 set CMAKE_PREFIX_PATH=%QT4%;%QT5%
+set PATH=%CMAKEHOME%
