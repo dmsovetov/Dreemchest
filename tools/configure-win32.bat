e33b0c2db8f50ddde81e6f677c339ce2c319177b
Dmitry Sovetov <dmsovetov@gmail.com>
2016-01-09 01:02:16 +0200

    Added: Relight dependency

--- tools/configure-win32.bat
+++ tools/configure-win32.bat
@@ -5,5 +5,5 @@ set PLATFORM=Windows
 call env.bat
 call select-project-folder.bat
 
-cmake ..\..\src -G "Visual Studio 12" -DDC_BUILD_TESTS=OFF -DDC_QT_SUPPORT=Auto -DDC_COMPOSER_ENABLED=ON -DTIFF_LIBRARY=../../externals/lib/Windows/120/Debug/tiffd.lib -DTIFF_INCLUDE_DIR=../../externals/include/tiff
+cmake ..\..\src -G "Visual Studio 12" -DDC_BUILD_TESTS=OFF -DDC_WITH_RELIGHT=ON -DDC_QT_SUPPORT=Auto -DDC_COMPOSER_ENABLED=ON -DEMBREE_INCLUDE_PATH=%EMBREE_ROOT%\include -DEMBREE_LIBRARY=%EMBREE_ROOT%\lib\embree.lib -DTIFF_LIBRARY=../../externals/lib/Windows/120/Debug/tiffd.lib -DTIFF_INCLUDE_DIR=../../externals/include/tiff
 pause
--- tools/env.bat
+++ tools/env.bat
@@ -6,4 +6,5 @@ set QT4=D:\SDK\Qt\4.8.6
 set QT5=D:\SDK\Qt\5.5.0
 set FBX_DIR=C:\Program Files\Autodesk\FBX\FBX SDK\2016.1.2
 set CMAKE_PREFIX_PATH=%QT4%;%QT5%
+set EMBREE_ROOT="C:\Program Files (x86)\embree2"
 set PATH=%CMAKEHOME%;%PATH%;..\..\externals\include;..\..\externals\lib\Windows\120\Debug
