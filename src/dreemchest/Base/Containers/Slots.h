3de3119630c3b5ab0c47620f4d0701a8516865af
Dmitry Sovetov <dmsovetov@gmail.com>
2016-01-15 14:52:20 +0200

    Refactored: Slots container

--- src/dreemchest/Base/Containers/Slots.h
+++ src/dreemchest/Base/Containers/Slots.h
@@ -29,139 +29,195 @@
 
 DC_BEGIN_DREEMCHEST
 
-    //! Forward declaration of a slot list.
-    template<typename TSlot> class SlotHandleList;
-
-    //! This handles are issued by slot containers to outer world.
+    //! Opaque handle type, stores array index & generation.
     template<s32 TBitsIndex = 12, s32 TBitsGeneration = 20>
-    class SlotHandle {
-    friend class SlotHandleList< SlotHandle<TBitsIndex, TBitsGeneration> >;
+    class OpaqueHandle {
     public:
 
-                    //! Copies an SlotHandle instance.
-                    SlotHandle( const SlotHandle& other )
-                        : index( other.index ), generation( other.generation ) {}
+                                //! Constructs OpaqueHandle instance.
+                                OpaqueHandle( u32 index = ~0, u32 generation = 0 )
+                                    : m_index( index ), m_generation( generation ) {}
 
-                    //! Casts this slot handle to an integer value.
-                    operator u32( void ) const { return index; }
+                                //! Copies an OpaqueHandle instance.
+                                OpaqueHandle( const OpaqueHandle& other )
+                                    : m_index( other.m_index ), m_generation( other.m_generation ) {}
 
-                    //! Returns true if this slot is valid.
-        bool        isValid( void ) const { return index != -1; }
+                                //! Casts this OpaqueHandle to an integer value.
+                                operator u32( void ) const { return m_index; }
+
+        //! Returns true if this handle is valid.
+        bool                    isValid( void ) const { return m_index != ~0; }
+
+        //! Returns opaque handle generation.
+        u32                     generation( void ) const { return m_generation; }
 
     private:
 
-                    //! Constructs SlotHandle instance.
-                    SlotHandle( u32 index = -1, u32 generation = 0 )
-                        : index( index ), generation( generation ) {}
-
-        u32         index       : TBitsIndex;       //!< Slot index that is referenced by this slot handle.
-        u32         generation  : TBitsGeneration;  //!< Generation of a slot at a time when this handle was issued.
+        u32                     m_index      : TBitsIndex;       //!< Slot index that is referenced by this handle.
+        u32                     m_generation : TBitsGeneration;  //!< Generation of a slot at a time when this handle was issued.
     };
 
-    //! Linked list of slot handles.
-    template<typename THandle>
-    class SlotHandleList {
+    //! Container issues opaque handles to it's slots that act as weak references.
+    template<typename TValue, typename THandle>
+    class Slots {
     public:
 
-                        //! Constructs SlotHandleList instance.
-                        SlotHandleList( s32 size );
-                        ~SlotHandleList( void );
+        typedef TValue          Value;  //!< Store the value type.
+        typedef THandle         Handle; //!< Store the handle type.
 
-        //! Expands the opaque handle list by count elements.
-        void            expand( s32 count );
+                                //! Constructs Slots instance.
+                                Slots( void );
 
-        //! Returns next free hande.
-        THandle         allocate( void );
+        //! Adds the value to a container and returns it's ID.
+        Handle                  add( const Value& value );
 
-        //! Frees the previously allocated handle.
-        bool            free( const THandle& handle );
+        //! Reserves the slot inside a container.
+        Handle                  reserve( void );
 
-        //! Returns true if the specified handle is still valid.
-        bool            isAlive( const THandle& handle ) const;
+        //! Removes item from a container with specified handle. Returns true if the removal succeed, otherwise returns false.
+        bool                    remove( const Handle& handle );
 
-        //! Returns the total number of allocated handles.
-        s32             allocatedCount( void ) const;
+        //! Returns true if the specified handle is valid.
+        bool                    has( const Handle& handle ) const;
+
+        //! Returns the value referenced by specified handle.
+        const Value&            get( const Handle& handle ) const;
+        Value&                  get( const Handle& handle );
+
+        //! Returns the total number of used slots.
+        s32                     size( void ) const;
 
         //! Returns the maxium capacity.
-        s32             capacity( void ) const;
+        s32                     capacity( void ) const;
 
         //! Returns the total number of free handles.
-        s32             freeCount( void ) const;
+        s32                     freeCount( void ) const;
 
     #ifdef DC_DEBUG
         //! Used for debuging, traces list of free slots.
-        void            trace( void ) const;
+        void                    traceFreeSlots( void ) const;
     #endif  /*  DC_DEBUG    */
 
     private:
 
-        THandle*        m_handles;  //!< An array of opaque handles.
-        u32             m_head;     //!< Head of a free list.
-        s32             m_count;    //!< The total number of allocated handles.
-        s32             m_capacity; //!< The maximum capacity.
+        //! Expands the opaque handle list by count elements.
+        void                    expand( s32 count );
+
+        //! Returns next free hande.
+        Handle                  allocate( void );
+
+    private:
+
+        Array<Value>            m_data;         //!< Actual slot data.
+        Array<Handle>           m_slots;        //!< Array of avaible slots.
+        u32                     m_head;         //!< Head of a free list.
+        s32                     m_count;        //!< The total number of allocated handles.
+        s32                     m_capacity;     //!< The maximum capacity.
     };
 
-    // ** SlotHandleList::SlotHandleList
-    template<typename THandle>
-    SlotHandleList<THandle>::SlotHandleList( s32 size ) : m_handles( NULL ), m_head( 0 ), m_count( 0 ), m_capacity( 0 )
+    // ** Slots::FixedSlots
+    template<typename TValue, typename THandle>
+    Slots<TValue, THandle>::Slots( void )
+         : m_head( 0 ), m_count( 0 ), m_capacity( 0 )
     {
         // Allocate handle pool.
-        if( size ) {
-            expand( size );
-        }
+        //if( size ) {
+        //    expand( size );
+        //}
     }
 
-    // ** SlotHandleList::~SlotHandleList
-    template<typename THandle>
-    SlotHandleList<THandle>::~SlotHandleList( void )
+    // ** Slots::size
+    template<typename TValue, typename THandle>
+    s32 Slots<TValue, THandle>::size( void ) const
     {
-        delete[]m_handles;
+        return m_count;
     }
 
-    // ** SlotHandleList::expand
-    template<typename THandle>
-    void SlotHandleList<THandle>::expand( s32 count )
+    // ** Slots::add
+    template<typename TValue, typename THandle>
+    THandle Slots<TValue, THandle>::add( const TValue& value )
+    {
+        // Maximum capacity reached - expand
+        if( freeCount() == 0 ) {
+            s32 count = static_cast<s32>( capacity() * 0.25f );
+            expand( count <= 0 ? 4 : count );
+        }
+
+        // Reserve the slot
+        Handle handle = allocate();
+        DC_BREAK_IF( !handle.isValid() );
+
+        // Copy value to a slot
+        m_data[handle] = value;
+
+        return handle;
+    }
+
+    // ** Slots::has
+    template<typename TValue, typename THandle>
+    bool Slots<TValue, THandle>::has( const THandle& handle ) const
+    {
+        if( !handle.isValid() ) {
+            return false;
+        }
+
+        DC_BREAK_IF( handle >= static_cast<u32>( capacity() ) );
+        return handle.generation() == m_slots[handle].generation();
+    }
+
+    // ** Slots::get
+    template<typename TValue, typename THandle>
+    const TValue& Slots<TValue, THandle>::get( const THandle& handle ) const
+    {
+        DC_BREAK_IF( !has( handle ) );
+        return m_data[handle];
+    }
+
+    // ** Slots::get
+    template<typename TValue, typename THandle>
+    TValue& Slots<TValue, THandle>::get( const THandle& handle )
+    {
+        DC_BREAK_IF( !has( handle ) );
+        return m_data[handle];
+    }
+
+    // ** Slots::expand
+    template<typename TValue, typename THandle>
+    void Slots<TValue, THandle>::expand( s32 count )
     {
         // Save previous capacity.
         s32 oldCapacity = capacity();
         m_capacity = oldCapacity + count;
 
-        // Allocate the block of handles.
-        THandle* handles = new THandle[capacity()];
+        // Resize the block of slots
+        m_slots.resize( capacity() );
 
-        // Copy old handles.
-        if( m_handles ) {
-            memcpy( handles, m_handles, sizeof( THandle ) * oldCapacity );
-        }
+        // Resize data
+        m_data.resize( capacity() );
 
-        // Add added handles to a free list.
+        // Add new handles to a free list.
         for( s32 i = oldCapacity; i < oldCapacity + count; i++ ) {
-            handles[i].index = i + 1;
+            m_slots[i] = i + 1;
         }
-
-        // Reset handles.
-        delete[]m_handles;
-        m_handles = handles;
     }
 
-    // ** SlotHandleList::allocate
-    template<typename THandle>
-    THandle SlotHandleList<THandle>::allocate( void )
+    // ** Slots::allocate
+    template<typename TValue, typename THandle>
+    THandle Slots<TValue, THandle>::allocate( void )
     {
         // Return invalid handle if no more slots left.
-        if( allocatedCount() >= capacity() ) {
-            DC_BREAK;
-            return THandle();
+        if( size() >= capacity() ) {
+            return 0;
         }
 
         // Get the first free handle.
-        THandle handle = m_handles[m_head];
+        THandle free = m_slots[m_head];
 
         // Save next free slot index.
-        u32 next = handle.index;
+        u32 next = free;
 
         // Fix the index.
-        handle.index = m_head;
+        free = THandle( m_head, free.generation() );
 
         // Remove it from a list.
         m_head = next;
@@ -169,25 +225,22 @@ DC_BEGIN_DREEMCHEST
         // Increase the size of a container.
         m_count++;
 
-        return handle;
+        return free;
     }
 
-    // ** SlotHandleList::free
-    template<typename THandle>
-    bool SlotHandleList<THandle>::free( const THandle& handle )
+    // ** SlotHandleList::remove
+    template<typename TValue, typename THandle>
+    bool Slots<TValue, THandle>::remove( const THandle& handle )
     {
-        if( !isAlive( handle ) ) {
+        if( !has( handle ) ) {
             return false;
         }
 
-        u32 idx = handle.index;
+        u32 idx = handle;
 
-        // Increase the generation counter to invalidate all living handles.
-        m_handles[idx].generation++;
-
-        // Place this handle to a free list.
-        m_handles[idx].index = m_head;
-        m_head               = idx;
+        // Place this handle to a free list and increase the generation counter to invalidate all living handles.
+        m_slots[idx] = THandle( m_head, handle.generation() + 1 );
+        m_head       = idx;
 
         // Decrease the allocated counter.
         m_count--;
@@ -195,161 +248,35 @@ DC_BEGIN_DREEMCHEST
         return true;
     }
 
-    // ** SlotHandleList::isAlive
-    template<typename THandle>
-    bool SlotHandleList<THandle>::isAlive( const THandle& handle ) const
-    {
-        DC_BREAK_IF( handle.index >= static_cast<u32>( capacity() ) );
-        return handle.generation == m_handles[handle].generation;
-    }
-
-    // ** SlotHandleList::allocatedCount
-    template<typename THandle>
-    s32 SlotHandleList<THandle>::allocatedCount( void ) const
-    {
-        return m_count;
-    }
-
     // ** SlotHandleList::freeCount
-    template<typename THandle>
-    s32 SlotHandleList<THandle>::freeCount( void ) const
+    template<typename TValue, typename THandle>
+    s32 Slots<TValue, THandle>::freeCount( void ) const
     {
-        return capacity() - allocatedCount();
+        return capacity() - size();
     }
 
     // ** SlotHandleList::capacity
-    template<typename THandle>
-    s32 SlotHandleList<THandle>::capacity( void ) const
+    template<typename TValue, typename THandle>
+    s32 Slots<TValue, THandle>::capacity( void ) const
     {
         return m_capacity;
     }
 
 #ifdef DC_DEBUG
-    // ** SlotHandleList::trace
-    template<typename THandle>
-    void SlotHandleList<THandle>::trace( void ) const
+    // ** Slots::traceFreeSlots
+    template<typename TValue, typename THandle>
+    void Slots<TValue, THandle>::traceFreeSlots( void ) const
     {
         printf( "Free slots: " );
 
         for( s32 idx = m_head; idx < capacity(); ) {
             printf( "%d ", idx );
-            idx = m_handles[idx].index;
+            idx = indexFromId( m_handles[idx] );
         }
 
         printf( "\n" );
     }
 #endif  /*  DC_DEBUG    */
-
-    //! Container issues opaque handles to it's slots that act as weak references.
-    template<typename TValue, s32 TBitsIndex = 12, s32 TBitsGeneration = 20>
-    class Slots {
-    public:
-
-        //! Store the value type.
-        typedef TValue  Value;
-
-        //! Alias the SlotHandle type.
-        typedef SlotHandle<TBitsIndex, TBitsGeneration> ID;
-
-                                //! Constructs Slots instance of specified size.
-                                Slots( s32 size = 0 );
-
-        //! Adds the value to a container and returns it's ID.
-        ID                      add( const TValue& value );
-
-        //! Removes item from a container with specified ID. Returns true if the removal succeed, otherwise returns false.
-        bool                    remove( const ID& id );
-
-        //! Returns true if the specified ID is valid.
-        bool                    has( const ID& id ) const;
-
-        //! Returns the value referenced by specified ID.
-        const TValue&           get( const ID& id ) const;
-        TValue&                 get( const ID& id );
-
-        //! Returns the total number of used slots.
-        s32                     size( void ) const;
-
-    #ifdef DC_DEBUG
-        //! Used for debuging, traces list of free slots.
-        void                    traceFreeSlots( void ) const { m_handles.trace(); }
-    #endif  /*  DC_DEBUG    */
-
-    protected:
-
-        Array<TValue>           m_data;         //!< Actual slot data.
-        SlotHandleList<ID>      m_handles;      //!< Array of avaible slot handles.
-    };
-
-    // ** Slots::FixedSlots
-    template<typename TValue, s32 TBitsIndex, s32 TBitsGeneration>
-    Slots<TValue, TBitsIndex, TBitsGeneration>::Slots( s32 size ) : m_handles( size )
-    {
-        m_data.resize( size );
-    }
-
-    // ** Slots::size
-    template<typename TValue, s32 TBitsIndex, s32 TBitsGeneration>
-    s32 Slots<TValue, TBitsIndex, TBitsGeneration>::size( void ) const
-    {
-        return m_handles.allocatedCount();
-    }
-
-    // ** Slots::add
-    template<typename TValue, s32 TBitsIndex, s32 TBitsGeneration>
-    typename Slots<TValue, TBitsIndex, TBitsGeneration>::ID Slots<TValue, TBitsIndex, TBitsGeneration>::add( const TValue& value )
-    {
-        // Maximum capacity reached - expand
-        if( m_handles.freeCount() == 0 ) {
-            s32 count = static_cast<s32>( m_handles.capacity() * 0.25f );
-            m_handles.expand( count <= 0 ? 1 : count );
-            m_data.resize( m_handles.capacity() );
-        }
-
-        // Reserve the slot
-        ID id = m_handles.allocate();
-
-        // Copy value to a slot
-        if( id.isValid() ) {
-            m_data[id] = value;
-        }
-
-        return id;
-    }
-
-    // ** Slots::remove
-    template<typename TValue, s32 TBitsIndex, s32 TBitsGeneration>
-    bool Slots<TValue, TBitsIndex, TBitsGeneration>::remove( const ID& id )
-    {
-        return m_handles.free( id );
-    }
-
-    // ** Slots::has
-    template<typename TValue, s32 TBitsIndex, s32 TBitsGeneration>
-    bool Slots<TValue, TBitsIndex, TBitsGeneration>::has( const ID& id ) const
-    {
-        if( !id.isValid() ) {
-            return false;
-        }
-
-        return m_handles.isAlive( id );
-    }
-
-    // ** Slots::get
-    template<typename TValue, s32 TBitsIndex, s32 TBitsGeneration>
-    const TValue& Slots<TValue, TBitsIndex, TBitsGeneration>::get( const ID& id ) const
-    {
-        DC_BREAK_IF( !has( id ) );
-        return m_data[id];
-    }
-
-    // ** Slots::get
-    template<typename TValue, s32 TBitsIndex, s32 TBitsGeneration>
-    TValue& Slots<TValue, TBitsIndex, TBitsGeneration>::get( const ID& id )
-    {
-        DC_BREAK_IF( !has( id ) );
-        return m_data[id];
-    }
 
 DC_END_DREEMCHEST
 
