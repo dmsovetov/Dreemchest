6b9d383279414f0bf82f0343b0c56f411d317a6e
Dmitry Sovetov <dmsovetov@gmail.com>
2016-04-14 14:40:31 +0300

    Fixed: usage of NIMBLE_NO_DEBUG macro

--- Source/Dreemchest/Deprecated.h
+++ Source/Dreemchest/Deprecated.h
@@ -74,14 +74,14 @@ DC_END_DREEMCHEST
                 }
 #endif  /*  NIMBLE_DEBUG    */
 
-#ifdef NIMBLE_NO_DEBUG
+#if NIMBLE_NO_DEBUG
     #undef DC_BREAK_IF
     #define DC_BREAK_IF( ... )
     #undef DC_ABORT_IF
     #define DC_ABORT_IF( ... )
     #undef DC_NOT_IMPLEMENTED
     #define DC_NOT_IMPLEMENTED
-#endif  /*  #ifdef NIMBLE_NO_DEBUG  */
+#endif  /*  #if NIMBLE_NO_DEBUG  */
 
 #define DC_DEPRECATED       NIMBLE_DEPRECATED
 #define DC_DECL_OVERRIDE    NIMBLE_OVERRIDE
