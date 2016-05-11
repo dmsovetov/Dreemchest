9eff98f4418228d0df442e6b78d2e7a983569705
Dmitry Sovetov <dmsovetov@gmail.com>
2016-05-11 13:54:02 +0300

    Added: streamed render pass to generate a rendered mesh on a CPU side

---
+++ Source/Dreemchest/Scene/Rendering/RenderSystem/StreamedRenderPass.cpp
@@ -0,0 +1,236 @@
+/**************************************************************************
+
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Dmitry Sovetov
+
+ https://github.com/dmsovetov
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ **************************************************************************/
+
+#include "StreamedRenderPass.h"
+
+DC_BEGIN_DREEMCHEST
+
+namespace Scene {
+
+// ** StreamedRenderPassBase::StreamedRenderPassBase
+StreamedRenderPassBase::StreamedRenderPassBase( RenderingContext& context, RenderScene& renderScene, s32 maxVerticesInBatch )
+    : RenderPassBase( context, renderScene )
+    , m_maxVerticesInBatch( maxVerticesInBatch )
+    , m_vertexFormat( VertexFormat::Position | VertexFormat::Color )
+{
+	// Request an input layout for a 2D vertex format
+	m_inputLayout  = m_context.requestInputLayout( m_vertexFormat );
+
+	// Request a vertex buffer used for rendering
+	m_vertexBuffer = m_context.requestVertexBuffer( NULL, m_maxVerticesInBatch * m_vertexFormat.vertexSize() );
+}
+
+// ** StreamedRenderPassBase::end
+void StreamedRenderPassBase::end( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack )
+{
+    flush( commands, stateStack );
+}
+
+// ** StreamedRenderPassBase::beginBatch
+void StreamedRenderPassBase::beginBatch( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, Renderer::PrimitiveType primitive, s32 capacity )
+{
+    // Flush an active batch
+    if( m_activeBatch.primitive != primitive ) {
+        flush( commands, stateStack );
+    }
+
+    // Calculate a batch capacity
+    s32 actualCapacity = capacity ? capacity : m_maxVerticesInBatch;
+
+    // And start a new one
+    m_activeBatch.size      = 0;
+    m_activeBatch.capacity  = actualCapacity;
+    m_activeBatch.primitive = primitive;
+    m_activeBatch.stream    = frame.allocate( m_vertexFormat.vertexSize() * actualCapacity );
+}
+
+// ** StreamedRenderPassBase::restartBatch
+void StreamedRenderPassBase::restartBatch( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, Renderer::PrimitiveType primitive )
+{
+    // Save an active batch state
+    ActiveBatch activeBatch = m_activeBatch;
+
+    // Flush an active batch
+    flush( commands, stateStack );
+
+    // And start a new one with same parameters
+    beginBatch( frame, commands, stateStack, primitive, activeBatch.capacity );
+}
+
+// ** StreamedRenderPassBase::flush
+void StreamedRenderPassBase::flush( RenderCommandBuffer& commands, RenderStateStack& stateStack )
+{
+    // Nothing to render - just skip
+    if( m_activeBatch.size == 0 ) {
+        return;
+    }
+
+    // Push a render state
+    StateScope state = stateStack.newScope();
+	state->bindVertexBuffer( m_vertexBuffer );
+	state->bindInputLayout( m_inputLayout );
+
+	// Upload vertex data to a GPU buffer and emit a draw primitives command
+	commands.uploadVertexBuffer( m_vertexBuffer, m_activeBatch.stream, m_activeBatch.size * m_vertexFormat.vertexSize() );
+	commands.drawPrimitives( 0, Renderer::PrimLines, stateStack.states(), 0, m_activeBatch.size );
+
+    // Reset an active batch state
+    m_activeBatch = ActiveBatch();
+}
+
+// ** StreamedRenderPassBase::hasEnoughSpace
+bool StreamedRenderPassBase::hasEnoughSpace( s32 additionalVertices ) const
+{
+    return (m_activeBatch.size + additionalVertices) <= m_activeBatch.capacity;
+}
+
+// ** StreamedRenderPassBase::emitFrustum
+void StreamedRenderPassBase::emitFrustum( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, f32 fov, f32 aspect, f32 near, f32 far, const Matrix4& transform, const Rgba& color )
+{
+    // Calculate a tangent of a fov angle
+    f32 fovTan2 = tanf( radians( fov * 0.5f ) );
+
+    // Calculate near and far planes size
+    f32 nh = fovTan2 * near;
+    f32 nw = nh * aspect;
+    f32 fh = fovTan2 * far;
+    f32 fw = fh * aspect;
+
+    // Construct a world space frustum vertices
+    Vec3 worldSpaceVertices[] = {
+          // Near plane
+          transform * Vec4( -nw, -nh, -near )
+        , transform * Vec4(  nw, -nh, -near )
+        , transform * Vec4(  nw,  nh, -near )
+        , transform * Vec4( -nw,  nh, -near )
+
+          // Far plane
+        , transform * Vec4( -fw, -fh, -far )
+        , transform * Vec4(  fw, -fh, -far )
+        , transform * Vec4(  fw,  fh, -far )
+        , transform * Vec4( -fw,  fh, -far )
+
+          // An origin point
+        , transform * Vec4( 0.0f, 0.0f, 0.0f )
+    };
+
+    // Emit a near plane
+    emitLine( frame, commands, stateStack, worldSpaceVertices[0], worldSpaceVertices[1], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[1], worldSpaceVertices[2], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[2], worldSpaceVertices[3], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[3], worldSpaceVertices[0], color );
+
+    // Emit a far plane
+    emitLine( frame, commands, stateStack, worldSpaceVertices[4], worldSpaceVertices[5], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[5], worldSpaceVertices[6], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[6], worldSpaceVertices[7], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[7], worldSpaceVertices[4], color );
+
+    // Emit edges that are behind the near plane
+    emitLine( frame, commands, stateStack, worldSpaceVertices[8], worldSpaceVertices[0], color.transparent( 0.25f ) );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[8], worldSpaceVertices[1], color.transparent( 0.25f ) );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[8], worldSpaceVertices[2], color.transparent( 0.25f ) );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[8], worldSpaceVertices[3], color.transparent( 0.25f ) );
+
+    // Emit edges that are infront of the near plane
+    emitLine( frame, commands, stateStack, worldSpaceVertices[0], worldSpaceVertices[4], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[1], worldSpaceVertices[5], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[2], worldSpaceVertices[6], color );
+    emitLine( frame, commands, stateStack, worldSpaceVertices[3], worldSpaceVertices[7], color );
+}
+
+// ** StreamedRenderPassBase::emitVertices
+void StreamedRenderPassBase::emitVertices( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, Renderer::PrimitiveType primitive, const Vec3* positions, const Rgba* colors, s32 count )
+{
+    // Ensure that we have a space for these vertices
+    if( !hasEnoughSpace( count ) ) {
+        restartBatch( frame, commands, stateStack, primitive );
+    }
+
+    // Write each vertex to an output stream
+    for( s32 i = 0; i < count; i++ ) {
+        m_vertexFormat.setVertexAttribute( VertexFormat::Position, positions[i], m_activeBatch.stream, m_activeBatch.size + i );
+        m_vertexFormat.setVertexAttribute( VertexFormat::Color, colors[i].toInteger(), m_activeBatch.stream, m_activeBatch.size + i );
+    }
+
+    // Increase a batch size
+    m_activeBatch.size += count;
+}
+
+// ** StreamedRenderPassBase::emitLine
+void StreamedRenderPassBase::emitLine( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, const Vec3& start, const Vec3& end, const Rgba& color )
+{
+    Vec3 positions[] = { start, end   };
+    Rgba colors[]    = { color, color };
+    emitVertices( frame, commands, stateStack, Renderer::PrimLines, positions, colors, 2 );
+}
+
+// ** StreamedRenderPassBase::emitWireBounds
+void StreamedRenderPassBase::emitWireBounds( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, const Bounds& bounds, const Rgba& color )
+{
+    // Get a bounding box min and max values
+    const Vec3& min = bounds.min();
+    const Vec3& max = bounds.max();
+
+    // Construct an extents vectors
+    Vec3 x = Vec3( bounds.width(), 0.0f, 0.0f );
+    Vec3 y = Vec3( 0.0f, bounds.height(), 0.0f );
+    Vec3 z = Vec3( 0.0f, 0.0f, bounds.depth() );
+
+    // Emit a bottom side of a bounding box
+    emitLine( frame, commands, stateStack, min        , min + x,     color );
+    emitLine( frame, commands, stateStack, min + x    , min + x + z, color );
+    emitLine( frame, commands, stateStack, min + x + z, min + z,     color );
+    emitLine( frame, commands, stateStack, min + z    , min,         color );
+
+    // Emit a top side of a bounding box
+    emitLine( frame, commands, stateStack, min + y        , min + x + y,     color );
+    emitLine( frame, commands, stateStack, min + x + y    , min + x + z + y, color );
+    emitLine( frame, commands, stateStack, min + x + z + y, min + z + y,     color );
+    emitLine( frame, commands, stateStack, min + z + y    , min + y,         color );
+
+    // Emit vertical lines
+    emitLine( frame, commands, stateStack, min        , min + y,         color );
+    emitLine( frame, commands, stateStack, min + x    , min + x + y,     color );
+    emitLine( frame, commands, stateStack, min + x + z, min + x + z + y, color );
+    emitLine( frame, commands, stateStack, min + z    , min + z + y,     color );
+}
+
+// ** StreamedRenderPassBase::emitBasis
+void StreamedRenderPassBase::emitBasis( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, const Matrix4& transform )
+{
+    // Calculate a transformed basis origin
+    Vec3 origin = transform * Vec3::zero();
+
+    // Emit a line for each basis vector
+    emitLine( frame, commands, stateStack, origin, Vec3( transform * Vec3::axisX() ), Rgba( 1.0f, 0.0f, 0.0f ) );
+    emitLine( frame, commands, stateStack, origin, Vec3( transform * Vec3::axisY() ), Rgba( 0.0f, 1.0f, 0.0f ) );
+    emitLine( frame, commands, stateStack, origin, Vec3( transform * Vec3::axisZ() ), Rgba( 0.0f, 0.0f, 1.0f ) );
+}
+
+} // namespace Scene
+
+DC_END_DREEMCHEST
---
+++ Source/Dreemchest/Scene/Rendering/RenderSystem/StreamedRenderPass.h
@@ -0,0 +1,147 @@
+/**************************************************************************
+
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Dmitry Sovetov
+
+ https://github.com/dmsovetov
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ **************************************************************************/
+
+#ifndef __DC_Scene_Rendering_StreamedRenderPass_H__
+#define __DC_Scene_Rendering_StreamedRenderPass_H__
+
+#include "RenderPass.h"
+
+DC_BEGIN_DREEMCHEST
+
+namespace Scene {
+
+	//! Streamed render pass generates a mesh vertex and index buffers on a CPU side and submits it to GPU upon a state change.
+	class StreamedRenderPassBase : public RenderPassBase {
+	public:
+
+                                        //! Constructs StreamedRenderPassBase instance.
+                                        StreamedRenderPassBase( RenderingContext& context, RenderScene& renderScene, s32 maxVerticesInBatch );
+
+        //! Flushes a generated vertex stream.
+        virtual void                    end( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack ) NIMBLE_OVERRIDE;
+
+    public:
+
+        //! Writes transformed vertices of a single line instance.
+        void                            emitLine( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, const Vec3& start, const Vec3& end, const Rgba& color );
+
+        //! Writes a wire box to an output stream.
+        void                            emitWireBounds( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, const Bounds& bounds, const Rgba& color );
+
+        //! Writes a frustum to an output stream.
+        void                            emitFrustum( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, f32 fov, f32 aspect, f32 near, f32 far, const Matrix4& transform, const Rgba& color );
+
+        //! Writes a basis to an output stream.
+        void                            emitBasis( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, const Matrix4& transform );
+
+        //! Writes a set of vertices to an output stream.
+        void                            emitVertices( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, Renderer::PrimitiveType primitive, const Vec3* positions, const Rgba* colors, s32 count );
+
+        //! Begins a new batch.
+        void                            beginBatch( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, Renderer::PrimitiveType primitive, s32 capacity );
+
+        //! Restarts a batch with a same parameters by flushing an active buffer.
+        void                            restartBatch( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, Renderer::PrimitiveType primitive );
+
+        //! Flushes an active batch.
+        void                            flush( RenderCommandBuffer& commands, RenderStateStack& stateStack );
+
+        //! Returns true if a batch can hold an additional amount of vertices.
+        bool                            hasEnoughSpace( s32 additionalVertices ) const;
+    
+	private:
+
+        //! A helper struct to hold an active batch state
+        struct ActiveBatch {
+                                        //! Constructs an ActiveBatch instance.
+                                        ActiveBatch( void )
+                                            : primitive( Renderer::TotalPrimitiveTypes )
+                                            , size( 0 )
+                                            , capacity( 0 )
+                                            , stream( NULL )
+                                            {
+                                            }
+
+            Renderer::PrimitiveType     primitive;              //!< An active primitive type.
+            s32                         size;                   //!< A total number of vertices written to a stream.
+            s32                         capacity;               //!< A maximum number of vertices that can be written to a stream.
+            void*                       stream;                 //!< A batch vertex stream.
+        };
+
+		VertexFormat					m_vertexFormat;		    //!< A debug vertex format.
+		RenderResource					m_vertexBuffer;		    //!< An intermediate vertex buffer used for batching.
+		RenderResource					m_inputLayout;		    //!< An input layout constructed from a vertex format.
+        s32                             m_maxVerticesInBatch;   //! A maximum number of vertices that can be rendered in a single batch
+        mutable ActiveBatch             m_activeBatch;          //!< An active batch state.
+	};
+
+	//! Generic render pass class.
+	template<typename TRenderable>
+	class StreamedRenderPass : public StreamedRenderPassBase {
+	public:
+
+							        //! Constructs StreamedRenderPass instance.
+							        StreamedRenderPass( RenderingContext& context, RenderScene& renderScene, s32 maxVerticesInBatch );
+
+		//! Processes an entity family and generates a vertex stream from it.
+		virtual void		        emitRenderOperations( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack ) NIMBLE_OVERRIDE;
+
+	protected:
+
+		//! Emits render operation for a single renderable entity.
+		virtual void		        emitRenderOperations( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack, const Ecs::Entity& entity, const TRenderable& renderable, const Transform& transform ) NIMBLE_ABSTRACT;
+
+	protected:
+
+		Ecs::IndexPtr		        m_index;        //!< Entity index to be processed.
+	};
+
+	// ** StreamedRenderPass::StreamedRenderPass
+	template<typename TRenderable>
+	StreamedRenderPass<TRenderable>::StreamedRenderPass( RenderingContext& context, RenderScene& renderScene, s32 maxVerticesInBatch )
+        : StreamedRenderPassBase( context, renderScene, maxVerticesInBatch )
+	{
+		m_index = renderScene.scene()->ecs()->requestIndex( "StreamedRenderPass<>", Ecs::Aspect::all<TRenderable, Transform>() );
+	}
+
+	// ** StreamedRenderPass::emitRenderOperations
+	template<typename TRenderable>
+	void StreamedRenderPass<TRenderable>::emitRenderOperations( RenderFrame& frame, RenderCommandBuffer& commands, RenderStateStack& stateStack )
+	{
+        // Get the entity set from index
+		const Ecs::EntitySet& entities = m_index->entities();
+
+        // Process each entity
+		for( Ecs::EntitySet::const_iterator i = entities.begin(), end = entities.end(); i != end; ++i ) {
+			emitRenderOperations( frame, commands, stateStack, *i->get(), *(*i)->get<TRenderable>(), *(*i)->get<Transform>() );
+		}
+	}
+
+} // namespace Scene
+
+DC_END_DREEMCHEST
+
+#endif    /*    !__DC_Scene_Rendering_StreamedRenderPass_H__    */
