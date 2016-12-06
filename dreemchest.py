a3f5f083ccbff58960f4c97412cefd99dd081058
Dmitry Sovetov <dmsovetov@gmail.com>
2016-12-06 14:41:30 +0200

    Added: command line tool to build third party libraries

--- dreemchest.py
+++ dreemchest.py
@@ -28,6 +28,7 @@
 
 import argparse
 import os
+import subprocess
 
 
 def enable_option(value):
@@ -38,6 +39,37 @@ def enable_option(value):
 def disable_option(value):
     """Converts a boolean option to a CMake ON/OFF switch"""
     return 'OFF' if value else 'ON'
+
+
+def cmake_generate(generator, source, output, parameters, rest):
+    """Invokes a CMake command to generate a build system"""
+
+    # Create an output directory
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    # Generate CMake parameter string
+    cmd_args = ['-D%s=%s' % (key, parameters[key]) for key in parameters]
+
+    # Generate a command line string
+    command_line = 'cmake -E chdir %s cmake %s -G "%s" %s %s' % (output, os.path.abspath(source),
+                                                                 generator,
+                                                                 ' '.join(cmd_args),
+                                                                 rest
+                                                                 )
+
+    with open(os.devnull, 'wb') as devnull:
+        subprocess.check_call(command_line, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
+
+
+def cmake_build(source, target, configuration):
+    """Build a CMake-generated project binary tree."""
+
+    # Generate a command line string
+    command_line = 'cmake --build %s --target %s --config %s' % (source, target, configuration)
+
+    with open(os.devnull, 'wb') as devnull:
+        subprocess.check_call(command_line, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
 
 
 class CommandLineTool:
@@ -53,23 +85,39 @@ class CommandLineTool:
         cls(self._commands.add_parser(name))
 
 
-class PlatformConfigurationCommand:
-    """A base class for all platform configuration commands"""
+class CMakeCommand:
+    """A base class for all CMake commands"""
 
-    def __init__(self, parser, rendering_backend):
+    def __init__(self, parser, source_dir='.', output_dir='.'):
         """Constructs a platform configuration command"""
 
         parser.add_argument('--source',
                             help='a source directory to generate build system.',
-                            default='.')
+                            default=source_dir)
         parser.add_argument('--output',
                             help='an output directory to place generated build system.',
-                            default='.')
+                            default=output_dir)
         parser.add_argument('--cpp',
                             help='specifies the C++ standard whose features that are required by a build system.',
                             type=int,
                             choices=[98, 11, 14],
                             default=98)
+
+        parser.set_defaults(function=self.configure)
+
+    def configure(self, options):
+        """Performs basic build system configuration"""
+        pass
+
+
+class PlatformConfigurationCommand(CMakeCommand):
+    """A base class for all platform configuration commands"""
+
+    def __init__(self, parser, rendering_backend):
+        """Constructs a platform configuration command"""
+
+        CMakeCommand.__init__(self, parser)
+
         parser.add_argument('--pch',
                             help='generate a build system that uses precompiled headers.',
                             action='store_true',
@@ -134,10 +182,6 @@ class PlatformConfigurationCommand:
     def _prepare(options):
         """Generates a command line arguments from an input options"""
 
-        # Create an output directory
-        if not os.path.exists(options.output):
-            os.makedirs(options.output)
-
         # Generate platform independent CMake arguments from passed options
         return dict(
             DC_USE_PCH=enable_option(options.pch),
@@ -149,15 +193,6 @@ class PlatformConfigurationCommand:
             CMAKE_CXX_STANDARD=options.cpp,
             DC_QT_SUPPORT=('disabled' if options.no_qt else options.qt).capitalize()
         )
-
-    @staticmethod
-    def _generate_build_system(generator, options, parameters):
-        """Invokes a CMake command to generate a build system"""
-
-        # Generate CMake parameter string
-        cmake_parameters = ['-D%s=%s' % (key, parameters[key]) for key in parameters]
-
-        os.system('cmake -E chdir %s cmake %s -G "%s" %s' % (args.output, os.path.abspath(args.source), generator, ' '.join(cmake_parameters)))
 
 
 class DesktopConfigureCommand(PlatformConfigurationCommand):
@@ -210,10 +245,10 @@ class MacOSConfigureCommand(DesktopConfigureCommand):
         """Performs MacOS build system configuration"""
 
         # Perform basic build configuration
-        cmake = self._prepare(options)
+        cmake_parameters = self._prepare(options)
 
         # Invoke a CMake command to generate a build system
-        self._generate_build_system('Xcode', options, cmake)
+        cmake_generate('Xcode', options.source, options.output, cmake_parameters)
 
 
 class ConfigureCommand(CommandLineTool):
@@ -235,6 +270,135 @@ class CheckoutCommand:
         """Constructs a pull command line tool"""
 
 
+class InstallCommand(CMakeCommand):
+    """A command line tool to build and install third party libraries bundled with a source code distribution"""
+
+    def __init__(self, parser):
+        """Constructs a third party install command"""
+
+        CMakeCommand.__init__(self, parser, output_dir='Externals/Prebuilt/MacOS')
+
+        self._add_library(parser, 'zlib')
+        self._add_library(parser, 'Box2D')
+        self._add_library(parser, 'OpenAL')
+        self._add_library(parser, 'GTest')
+        self._add_library(parser, 'jsoncpp')
+        self._add_library(parser, 'libpng')
+        self._add_library(parser, 'libtiff')
+        self._add_library(parser, 'lua')
+        self._add_library(parser, 'ogg')
+        self._add_library(parser, 'vorbis')
+
+        parser.set_defaults(function=self.install)
+
+    def install(self, options):
+        install_path = os.path.abspath(options.output)
+
+        if not options.no_zlib:
+            self._build_and_install('zlib',
+                                    'Xcode',
+                                    'Externals/zlib',
+                                    install_path,
+                                    dict()
+                                    )
+
+        if not options.no_box2d:
+            self._build_and_install('Box2D',
+                                    'Xcode',
+                                    'Externals/Box2D/Box2D',
+                                    install_path,
+                                    dict(BOX2D_BUILD_EXAMPLES=False)
+                                    )
+
+        if not options.no_jsoncpp:
+            self._build_and_install('jsoncpp',
+                                    'Xcode',
+                                    'Externals/jsoncpp',
+                                    install_path,
+                                    dict(JSONCPP_WITH_TESTS=False, JSONCPP_WITH_POST_BUILD_UNITTEST=False)
+                                    )
+
+        if not options.no_ogg:
+            self._build_and_install('ogg',
+                                    'Xcode',
+                                    'Externals/ogg',
+                                    install_path,
+                                    dict())
+
+        if not options.no_vorbis:
+            self._build_and_install('vorbis',
+                                    'Xcode',
+                                    'Externals/vorbis',
+                                    install_path,
+                                    dict())
+
+        if not options.no_gtest:
+            self._build_and_install('GoogleTest',
+                                    'Xcode',
+                                    'Externals/GoogleTest',
+                                    install_path,
+                                    dict())
+
+        if not options.no_libpng:
+            self._build_and_install('libpng',
+                                    'Xcode',
+                                    'Externals/libpng',
+                                    install_path,
+                                    dict(PNG_TESTS=False))
+
+        if not options.no_libtiff:
+            self._build_and_install('libtiff',
+                                    'Xcode',
+                                    'Externals/libtiff',
+                                    install_path,
+                                    dict())
+
+        if not options.no_lua:
+            self._build_and_install('lua',
+                                    'Xcode',
+                                    'Externals/lua',
+                                    install_path,
+                                    dict(BUILD_SHARED_LIBS=False))
+
+        if not options.no_openal:
+            self._build_and_install('OpenAL',
+                                    'Xcode',
+                                    'Externals/OpenAL',
+                                    install_path,
+                                    dict(ALSOFT_EXAMPLES=False, ALSOFT_TESTS=False, LIBTYPE='STATIC'))
+
+    @staticmethod
+    def _build_and_install(library, generator, source, output, options):
+        """Generates a binary tree, then builds it and installs to a specified destination folder"""
+
+        # Set the CMAKE_INSTALL_PREFIX variable
+        options['CMAKE_INSTALL_PREFIX:PATH'] = output
+
+        # Generate a binary tree path
+        binary_dir = 'Projects/Externals/%s' % library
+
+        print '%s => %s' % (library, output)
+
+        try:
+            # Generate a binary tree
+            cmake_generate(generator, source, binary_dir, options, '-Wno-dev')
+
+            # Build a generated tree
+            cmake_build(binary_dir, 'install', 'Release')
+
+        except subprocess.CalledProcessError:
+            print 'Failed to build %s' % library
+
+    @staticmethod
+    def _add_library(parser, name):
+        """Adds a third party build options"""
+        parser.add_argument('--no-%s' % name.lower(),
+                            help='do not build a %s.' % name,
+                            action='store_true',
+                            default=False
+                            )
+
+
 class Main(CommandLineTool):
     """An entry point to a command line tool"""
 
@@ -245,6 +409,7 @@ class Main(CommandLineTool):
 
         self._add_command('configure', ConfigureCommand)
         self._add_command('checkout', CheckoutCommand)
+        self._add_command('install', InstallCommand)
 
 # Entry point
 if __name__ == "__main__":
