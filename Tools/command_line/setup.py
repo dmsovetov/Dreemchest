71dbd92a4ca6c87ebbb2bc00a1f06ab08f8320e3
Dmitry Sovetov <dmsovetov@gmail.com>
2016-12-07 17:28:33 +0200

    Updated: command line tool version

--- Tools/command_line/setup.py
+++ Tools/command_line/setup.py
@@ -27,7 +27,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dreemchest',
-      version='0.25',
+      version='0.26',
       description='Dreemchest engine command line tool.',
       url='https://github.com/dmsovetov/Dreemchest',
       author='Dmitry Sovetov',
