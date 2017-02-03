aafc889b516be71444cae09dfb5c0e283fd6b3e9
dmsovetov <dmsovetov@gmail.com>
2017-02-03 22:55:44 +0200

    Fixed: Windows compilation error

--- Source/Dreemchest/Renderer/RenderingContext.cpp
+++ Source/Dreemchest/Renderer/RenderingContext.cpp
@@ -146,7 +146,7 @@ RenderingContext::RenderingContext(RenderViewPtr view)
     LogDebug("renderingContext", "rendering state size is %d bytes\n", sizeof(State));
     LogDebug("renderingContext", "rendering state block size is %d bytes\n", sizeof(StateBlock));
     LogDebug("renderingContext", "opcode size is %d bytes\n", sizeof(OpCode));
-    LogDebug("renderingContext", "drawCall size is %d bytes\n", sizeof(OpCode::drawCall));
+//    LogDebug("renderingContext", "drawCall size is %d bytes\n", sizeof(OpCode::drawCall));
 
     // Create a construction command buffer instance
     m_resourceCommandBuffer = DC_NEW ResourceCommandBuffer;
