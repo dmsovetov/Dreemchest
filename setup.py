d9e58001b2b57733fdfa160b7b58c437157a9fd4
Dmitry Sovetov <dmsovetov@gmail.com>
2017-02-09 15:07:18 +0200

    Finished: DR-3 / Environment configuration command

--- setup.py
+++ setup.py
@@ -30,16 +30,19 @@ import os
 import urllib2
 import tarfile
 import zipfile
-import shutil
 import subprocess
+import shutil
 from collections import namedtuple
-
 
 DREEMCHEST_HOME = 'DREEMCHEST_HOME'
 DREEMCHEST_CMAKE = 'DREEMCHEST_CMAKE'
-GENERATE_COMMAND = '{cmake}/cmake -E chdir {output} {cmake}/cmake {source} -G "{generator}" {args} {rest}'
+DREEMCHEST_CMAKE_BIN = 'DREEMCHEST_CMAKE_BIN'
+DREEMCHEST_ANDROID = 'DREEMCHEST_ANDROID'
+DREEMCHEST_EMSCRIPTEN = 'DREEMCHEST_EMSCRIPTEN'
+ANDROID = 'ANDROID_HOME'
+EMSCRIPTEN = 'EMSCRIPTEN'
 
-EnvironmentPaths = namedtuple('EnvironmentPaths', ['home', 'cmake'])
+EnvironmentPaths = namedtuple('EnvironmentPaths', ['home', 'cmake', 'android', 'emscripten'])
 
 
 def platform_name():
@@ -64,6 +67,11 @@ class Environment:
 
         self._paths = paths
         self.set_variable(DREEMCHEST_HOME, paths.home)
+        self.set_variable(DREEMCHEST_ANDROID, paths.android)
+        self.set_variable(DREEMCHEST_CMAKE, paths.cmake)
+
+        if paths.emscripten:
+            self.set_variable(DREEMCHEST_EMSCRIPTEN, paths.emscripten)
 
     def set_variable(self, name, value):
         """Sets an environment variable"""
@@ -74,10 +82,6 @@ class Environment:
         """Adds a specified path to an environment"""
 
         raise Exception('a path could not be added to an environment')
-
-    def cmake_path(self):
-        """Returns a CMake installation path"""
-        return self._paths.cmake
 
 
 class UnixEnvironment(Environment):
@@ -90,7 +94,6 @@ class UnixEnvironment(Environment):
 
         self._bash_files = [os.path.expanduser('~/' + bash_file) for bash_file in bash_files]
         self._bash_file = self.bash_file()
-        #self.backup_file(self._bash_file)
 
     def bash_file(self):
         """Returns a bash file name to be updated"""
@@ -114,21 +117,6 @@ class UnixEnvironment(Environment):
 
         Environment.configure(self, paths)
 
-    @staticmethod
-    def backup_file(file_name):
-        """Creates a backup file of a bash file to be modified"""
-
-        # Nothing to backup
-        if not os.path.exists(file_name):
-            return
-
-        backup_file_name = file_name + '.backup'
-
-        if os.path.exists(backup_file_name):
-            raise Exception('Dreemchest is already installed')
-
-        shutil.copy(file_name, backup_file_name)
-
     def set_variable(self, name, value):
         """Writes an environment variable to bash configuration file"""
 
@@ -140,17 +128,6 @@ class UnixEnvironment(Environment):
 
         self._append_to_bash('export PATH={path}:$PATH\n'.format(path=path))
         print 'Path {path} added to environment'.format(path=path)
-
-    def cmake_path(self):
-        """Returns a CMake installation path"""
-
-        paths = os.environ['PATH'].split(':')
-
-        for path in paths:
-            if os.path.exists(os.path.join(path, 'cmake')):
-                return path
-
-        return Environment.cmake_path(self)
 
     def uninstall(self):
         """Cleans up previous environment configuration"""
@@ -185,11 +162,12 @@ class MacOSEnvironment(UnixEnvironment):
 
         UnixEnvironment.configure(self, paths)
 
-        self.add_path('$' + DREEMCHEST_HOME)
+        # Append macOS bundle path
+        cmake_bin = os.path.join(paths.cmake, 'CMake.app', 'Contents', 'bin')
+        self.set_variable(DREEMCHEST_CMAKE_BIN, cmake_bin)
 
-        if paths.cmake:
-            self.set_variable(DREEMCHEST_CMAKE, os.path.join('$' + DREEMCHEST_HOME, paths.cmake, 'CMake.app/Contents/bin'))
-            self.add_path('$' + DREEMCHEST_CMAKE)
+        # Add DREEMCHEST_HOME variable to path
+        self.add_path('$' + DREEMCHEST_HOME)
 
 
 class WindowsEnvironment(Environment):
@@ -223,11 +201,12 @@ class WindowsEnvironment(Environment):
 
         Environment.configure(self, paths)
 
-        self.add_path('%' + DREEMCHEST_HOME + '%')
+        # Append CMake executable location
+        cmake_bin = os.path.join(paths.cmake, 'bin')
+        self.set_variable(DREEMCHEST_CMAKE_BIN, cmake_bin)
 
-        if paths.cmake:
-            self.set_variable(DREEMCHEST_CMAKE, os.path.join('%' + DREEMCHEST_HOME + '%', paths.cmake, 'CMake.app/Contents/bin'))
-            self.add_path('%' + DREEMCHEST_CMAKE + '%')
+        # Add DREEMCHEST_HOME to path
+        self.add_path('%' + DREEMCHEST_HOME + '%')
 
     def add_path(self, path):
         """Adds a specified path to an environment"""
@@ -294,8 +273,16 @@ def extract(file_name, path):
         tar.extractall(path)
         name, ext = os.path.splitext(name)
     elif ext == '.zip':
+        def extract_file(zf, info, extract_dir):
+            zf.extract(info.filename, path=extract_dir)
+            out_path = os.path.join(extract_dir, info.filename)
+
+            perm = info.external_attr >> 16L
+            os.chmod(out_path, perm)
+
         with zipfile.ZipFile(file_name, "r") as z:
-            z.extractall(path)
+            for info in z.infolist():
+                extract_file(z, info, path)
 
     os.remove(file_name)
 
@@ -347,46 +334,180 @@ def invoke(command):
     """Invokes a command as a subprocess"""
 
     try:
-        subprocess.check_call(command, shell=True, stdout=None, stderr=subprocess.STDOUT)
+        subprocess.check_call(command, shell=True)
         return True
     except subprocess.CalledProcessError:
         return False
+
+
+def install_cmake(version):
+    # Skip CMake installation if we already have a valid installation
+    if DREEMCHEST_CMAKE_BIN in os.environ.keys():
+        try:
+            cmake_path = os.environ[DREEMCHEST_CMAKE_BIN]
+            cmake_command = os.path.join(cmake_path, 'cmake --version')
+
+            if not os.path.exists(os.path.join(cmake_path, 'cmake')):
+                raise Exception('cmake executable does not exist')
+
+            result = subprocess.check_output(cmake_command, shell=True)
+            cmake_version = result.split("\n\n")[0].split(' ')[2]
+
+            if cmake_version == version:
+                print 'Found existing CMake %s executable at %s' % (cmake_version, cmake_path)
+                return cmake_path
+
+            print 'Found CMake %s, but %s was requested, downloading the required version...' % (cmake_version, version)
+        except subprocess.CalledProcessError:
+            print "Warning: 'DREEMCHEST_CMAKE' variable points to an invalid folder"
+        except Exception as err:
+            print "Warning: %s" % err
+
+    # Download and extract CMake
+    return download_cmake(version)
+
+
+def install_android(version, api_levels, architectures):
+    # Skip Android SDK installation if we already have a valid installation
+    if DREEMCHEST_ANDROID in os.environ.keys():
+        print 'Android SDK installation found at %s' % os.environ[DREEMCHEST_ANDROID]
+        return os.environ[DREEMCHEST_ANDROID]
+
+    if ANDROID in os.environ.keys():
+        print 'Android SDK installation found at %s' % os.environ[ANDROID]
+        return os.environ[ANDROID]
+
+    # Download an Android SDK
+    platform = {'windows': 'windows', 'linux': 'linux', 'mac': 'macosx'}[platform_name()]
+
+    # Format an SDK url and file name
+    url = 'https://dl.google.com/android/repository/tools_r{version}-{platform}.zip'.format(
+        version=version,
+        platform=platform
+    )
+
+    try:
+        file_name = 'android-sdk.zip'
+
+        # Download Android SDK
+        download(url, file_name)
+
+        # Extract Android SDK
+        android_path = extract(file_name, os.path.join('Tools', 'android-%s' % version))
+        android_path = android_path.replace('android-sdk', '')  # HOTFIX
+    except:
+        raise Exception('Android SDK could not be downloaded, no such version')
+
+    sdk_manager = os.path.join(android_path, 'tools', 'bin', 'sdkmanager')
+
+    # Install all required packages
+    for api_level in api_levels:
+        print 'Installing Android API level %s...' % api_level
+        if not invoke(sdk_manager + ' "platforms;android-%s"' % api_level):
+            raise Exception('Failed to install Android API level %s' % api_level)
+
+        for arch in architectures:
+            print 'Installing Android system image for %s architecture with API level %s' % (arch, api_level)
+            invoke(sdk_manager + ' "system-images;android-%s;default;%s"' % (api_level, arch))
+
+    print 'Installing Android tools...'
+    if not invoke(sdk_manager + ' tools'):
+        raise Exception('Failed to install Android tools')
+
+    print 'Installing Android platform tools...'
+    if not invoke(sdk_manager + ' platform-tools'):
+        raise Exception('Failed to install Android platform tools')
+
+    print 'Installing Android NDK...'
+    if not invoke(sdk_manager + ' ndk-bundle'):
+        raise Exception('Failed to install an Android NDK')
+
+    return android_path
+
+
+def install_emscripten(version):
+    # Skip Android SDK installation if we already have a valid installation
+    if DREEMCHEST_EMSCRIPTEN in os.environ.keys():
+        print 'Emscripten installation found at %s' % os.environ[DREEMCHEST_EMSCRIPTEN]
+        return os.environ[DREEMCHEST_EMSCRIPTEN]
+
+    if EMSCRIPTEN in os.environ.keys():
+        print 'Emscripten installation found at %s' % os.environ[EMSCRIPTEN]
+        return os.environ[EMSCRIPTEN]
+
+    # Download an Emscripten SDK
+    url = dict(
+        windows='https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-1.35.0-portable-64bit.zip',
+        linux='https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz',
+        mac='https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz',
+    )[platform_name()]
+
+    file_name = url.split('/')[-1]
+
+    download(url, file_name)
+
+    # Extract contents
+    emscripten_home = extract(file_name, 'Tools')
+    emscripten_home = emscripten_home.replace('emsdk-portable', 'emsdk_portable')  # HOTFIX
+
+    # Install Emscripten SDK
+    emsdk = os.path.join(emscripten_home, 'emsdk')
+    print 'Installing Emscripten SDK...'
+    invoke(emsdk + ' update')
+    invoke(emsdk + ' install latest')
+    invoke(emsdk + ' activate latest')
+
+    # Patch the Emscripten.cmake toolchain file
+    emscripten_path = os.path.join(emscripten_home, 'emscripten', '1.35.0')
+    source_toolchain = os.path.join('CMake', 'Toolchains', 'Emscripten.cmake')
+    dest_toolchain = os.path.join(emscripten_path, 'cmake', 'Modules', 'Platform', 'Emscripten.cmake')
+
+    shutil.copyfile(source_toolchain, dest_toolchain)
+
+    return emscripten_home
 
 
 def main():
     """A command line tool entry point"""
 
     parser = argparse.ArgumentParser(prog='setup', description='Dreemchest engine install tool')
+    parser.add_argument('--cmake', help='a CMake version to be installed.', type=str, default='3.4.3')
 
-    # CMake option
-    cmake_options = parser.add_mutually_exclusive_group()
-    cmake_options.add_argument(
-        '--no-cmake', help='skips a CMake installation', action='store_true', default=False
-    )
-    cmake_options.add_argument(
-        '--cmake', help='specifies a CMake version to be installed.', type=str, default='3.4.3'
-    )
+    android_options = parser.add_mutually_exclusive_group()
+    android_options.add_argument('--no-android',
+                                 help="skips an Android SDK installation",
+                                 action='store_true',
+                                 default=False)
+    android_options.add_argument('--android',
+                                 help='an Android SDK version to be installed',
+                                 type=str,
+                                 default='25.2.3')
+
+    emscripten_options = parser.add_mutually_exclusive_group()
+    emscripten_options.add_argument('--no-emscripten',
+                                 help="skips an Emscripten SDK installation",
+                                 action='store_true',
+                                 default=False)
+    emscripten_options.add_argument('--emscripten',
+                                    help='an Android SDK version to be installed',
+                                    action='store_true',
+                                    default=True)
 
     args = parser.parse_args()
 
     # Download CMake
-    cmake_path = None
+    cmake_path = install_cmake(args.cmake)
+    android_path = install_android(args.android, [24], ['arm64-v8a', 'armeabi-v7a', 'x86', 'x86_64'])
+    emscripten_path = install_emscripten('1.35.0')
 
-    if not args.no_cmake:
-        cmake_path = 'D:\\Projects\\github\\BFG9000\\src\\thirdParty\\Dreemchest\\tools\\cmake-3.4.3-win32-x86'
-    #    cmake_path = download_cmake(args.cmake)
-    #    print 'Installed CMake to ' + cmake_path
+    # Make all paths absolute
+    cmake_path = os.path.abspath(cmake_path)
+    android_path = os.path.abspath(android_path)
+    emscripten_path = os.path.abspath(emscripten_path)
 
     # First setup environment variables
-    paths = EnvironmentPaths(os.getcwd(), cmake_path)
+    paths = EnvironmentPaths(os.getcwd(), cmake_path, android_path, emscripten_path)
     env = configure_environment(args, paths)
-
-    cmake_install_path = env.cmake_path()
-
-    if cmake_install_path:
-        print 'CMake is located at', cmake_install_path
-    else:
-        raise Exception('CMake installation could not be found')
 
 if __name__ == "__main__":
     try:
